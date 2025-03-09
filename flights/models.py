f4bf67df1cd212fbbd94a10c25c6962a1493eed4
bushraqurabi <2qubushra@gmail.com>
2025-03-09 23:19:30 +0400

    Initial commit

---
+++ flights/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.origin} to {self.destination}"
