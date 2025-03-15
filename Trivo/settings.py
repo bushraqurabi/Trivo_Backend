261635169fff8da65f066194e9a5059b962a58a8
ck <m.cankisi@gmail.com>
2025-03-15 14:51:42 +0000

    fix: delete extra indentation

--- Trivo/settings.py
+++ Trivo/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'Trivo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
